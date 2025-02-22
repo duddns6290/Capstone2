95e9b8eaf4c8707f59f3f4c2b99ea361ae1ba606
duddns6290 <duddns6290@naver.com>
2025-02-22 16:17:32 +0900

    Controller 예시

--- src/main/java/org/capstone/controller/api.java
+++ src/main/java/org/capstone/controller/api.java
@@ -1,19 +1,19 @@
-package org.capstone.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class api {
-
-    private int counter = 0;
-
-    @GetMapping("/test")
-    public String get() {
-        counter++;
-        System.out.println("Hello World Console = " + counter);
-        return "Hello World View = " + counter;
-    }
-
-
-}
+//package org.capstone.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class api {
+//
+//    private int counter = 0;
+//
+//    @GetMapping("/test")
+//    public String get() {
+//        counter++;
+//        System.out.println("Hello World Console = " + counter);
+//        return "Hello World View = " + counter;
+//    }
+//
+//
+//}
